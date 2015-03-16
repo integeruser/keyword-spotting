2bb01c666d630d5a8c142cb978e67853f883373b
Francesco Cagnin <francesco.cagnin@gmail.com>
2015-03-16 20:43:42 +0100

    re-added contrast_threshold input parameter (it can be useful)

--- construct_corpus.py
+++ construct_corpus.py
@@ -6,12 +6,14 @@ import pickle
 import sys
 
 
-if len(sys.argv) != 2:
-    sys.exit('Usage: {0} pages_directory_path'.format(sys.argv[0]))
+if len(sys.argv) != 3:
+    sys.exit('Usage: {0} pages_directory_path contrast_threshold'.format(sys.argv[0]))
 
 pages_directory_path = sys.argv[1]
+contrast_threshold = float(sys.argv[2])
 print 'Starting script...'
 print '   {0: <20} = {1}'.format('pages_directory_path', pages_directory_path)
+print '   {0: <20} = {1}'.format('contrast_threshold', contrast_threshold)
 
 ################################################################################
 
@@ -20,7 +22,7 @@ print '   {0: <20} = {1}'.format('pages_directory_path', pages_directory_path)
 print 'Loading pages...'
 corpus = {'pages': list(), 'keypoints': list(), 'descriptors': list()}
 
-sift = cv2.SIFT()
+sift = cv2.SIFT(contrastThreshold=contrast_threshold)
 for page_file_path in os.listdir(pages_directory_path):
     print '   Detecting keypoints and computing descriptors on \'{0}\'...'.format(page_file_path)
     page = cv2.imread(pages_directory_path + '/' + page_file_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
@@ -37,5 +39,5 @@ for page_file_path in os.listdir(pages_directory_path):
 
 # save pages, keypoints and descriptors for later use
 print 'Saving corpus...'
-with open('corpus', 'wb') as f:
+with open('corpus-' + str(contrast_threshold), 'wb') as f:
     pickle.dump(corpus, f, protocol=pickle.HIGHEST_PROTOCOL)
