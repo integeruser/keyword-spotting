d315032ebd3500563ab67194e253982a9307ea68
Francesco Cagnin <francesco.cagnin@gmail.com>
2015-03-19 11:09:25 +0100

    added n_octave_layers to output file name

--- construct_corpus.py
+++ construct_corpus.py
@@ -45,6 +45,7 @@ for page_file_path in os.listdir(pages_directory_path):
 
 # save pages, keypoints and descriptors for later use
 print 'Saving corpus...'
-corpus_file_name = 'corpus-{0}-{1}'.format(len(corpus['pages']), contrast_threshold)
+corpus_file_name = 'corpus-{0}-{1}-{2}'.format(
+    len(corpus['pages']), contrast_threshold, n_octave_layers)
 with open(corpus_file_name, 'wb') as f:
     cPickle.dump(corpus, f, protocol=cPickle.HIGHEST_PROTOCOL)
