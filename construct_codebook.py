be2aed951ab06e3d883b55f87f8a56f477cb0f58
Francesco Cagnin <francesco.cagnin@gmail.com>
2015-03-21 18:07:25 +0100

    added epsilon parameter; minor changes (style)

--- construct_codebook.py
+++ construct_codebook.py
@@ -8,16 +8,18 @@ import sys
 import utils
 
 
-if len(sys.argv) != 4:
-    sys.exit('Usage: {0} corpus_file_path codebook_size max_iter'.format(sys.argv[0]))
+if len(sys.argv) != 5:
+    sys.exit('Usage: {0} corpus_file_path codebook_size max_iter epsilon'.format(sys.argv[0]))
 
 corpus_file_path = sys.argv[1]
 codebook_size = int(sys.argv[2])
 max_iter = int(sys.argv[3])
+epsilon = float(sys.argv[4])
 print 'Starting script...'
 print '   {0: <16} = {1}'.format('corpus_file_path', corpus_file_path)
 print '   {0: <16} = {1}'.format('codebook_size', codebook_size)
 print '   {0: <16} = {1}'.format('max_iter', max_iter)
+print '   {0: <16} = {1}'.format('epsilon', epsilon)
 
 ################################################################################
 
@@ -40,14 +42,11 @@ for page_keypoints, page_descriptors in itertools.izip(corpus['keypoints'], corp
 print 'Running k-means on the descriptors space...'
 corpus_descriptors_vstack = numpy.vstack(corpus['descriptors'])
 
-epsilon = 1.0
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, max_iter, epsilon)
 attempts = 10
 compactness, labels, d = cv2.kmeans(
     corpus_descriptors_vstack, codebook_size, criteria, attempts, cv2.KMEANS_RANDOM_CENTERS)
-# compactness: the sum of squared distance from each point to their corresponding centers
-# labels: the label array where each element marked '0', '1', ...
-# d: the array of centers of clusters
+assert len(d) == codebook_size
 
 
 # construct the codebook of k visual words
@@ -57,35 +56,34 @@ corpus_keypoints_vstack = numpy.asarray(
 
 codebook = list()
 # create a codeword for each k-means group found
-for group in range(codebook_size):
-    centroid = d[group]
+for group in range(len(d)):
+    codeword_centroid = d[group]
 
-    # find which keypoints belong to the current group
+    # find keypoints that belong to the current group, and group them in pages
     codeword_keypoints = dict.fromkeys(corpus['pages'], list())
     codeword_descriptors = dict.fromkeys(corpus['pages'], list())
 
-    curr_page_start_index = 0
-    curr_page_last_index = 0
+    curr_page_start_index = curr_page_last_index = 0
     for i, page in enumerate(corpus['pages']):
+        assert len(corpus['keypoints'][i]) == len(corpus['descriptors'][i])
         curr_page_last_index = curr_page_start_index + len(corpus['keypoints'][i])
 
         curr_page_keypoints = corpus_keypoints_vstack[
             curr_page_start_index:curr_page_last_index]
         curr_page_curr_group_keypoints = curr_page_keypoints[
             labels.ravel()[curr_page_start_index:curr_page_last_index] == group]
+        codeword_keypoints[page] = utils.serialize_keypoints(curr_page_curr_group_keypoints)
 
         curr_page_descriptors = corpus_descriptors_vstack[
             curr_page_start_index:curr_page_last_index]
         curr_page_curr_group_descriptors = curr_page_descriptors[
             labels.ravel()[curr_page_start_index:curr_page_last_index] == group]
-
-        codeword_keypoints[page] = utils.serialize_keypoints(curr_page_curr_group_keypoints)
         codeword_descriptors[page] = curr_page_curr_group_descriptors
 
         curr_page_start_index = curr_page_last_index
 
     codeword = {
-        'd': centroid,
+        'd': codeword_centroid,
         'keypoints': codeword_keypoints,
         'descriptors': codeword_descriptors
     }
@@ -102,7 +100,8 @@ assert sum([len(v) for codeword in codebook for v in codeword['descriptors'].val
 # save codebook for later use
 print 'Saving codebook...'
 contrast_threshold = re.findall(r'corpus-.*-([^(]*)-.*', corpus_file_path)[0]
-codebook_file_name = 'codebook-{0}-{1}-{2}-{3}'.format(
-    len(corpus['pages']), contrast_threshold, codebook_size, max_iter)
+n_octave_layers = corpus_file_path[corpus_file_path.rfind('-') + 1:]
+codebook_file_name = 'codebook-{0}-{1}-{2}--{3}-{4}-{5}'.format(
+    len(corpus['pages']), contrast_threshold, n_octave_layers, codebook_size, max_iter, epsilon)
 with open(codebook_file_name, 'wb') as f:
     cPickle.dump(codebook, f, protocol=cPickle.HIGHEST_PROTOCOL)
