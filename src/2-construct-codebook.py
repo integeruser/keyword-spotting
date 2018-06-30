4da648d119a1cb33162326e4596bcb6f6a2c59f6
Francesco Cagnin <francesco.cagnin@gmail.com>
2018-06-30 12:02:57 +0200

    Cleanup

--- src/2-construct-codebook.py
+++ src/2-construct-codebook.py
@@ -1,121 +1,99 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import argparse
 import hashlib
-import numpy
 import os
+import sys
+
+import numpy
 
 import cv2
 import utils
 
 
-def run(args, output_directory_path="", force=False):
-    corpus_file_path = args["corpus_file_path"]
-    codebook_size = int(args["codebook_size"])
-    max_iter = int(args["max_iter"])
-    epsilon = float(args["epsilon"])
+def run():
+    codebook_fingerprint = os.path.basename(args.corpus_filepath)
+    codebook_fingerprint += str(args.codebook_size)
+    codebook_fingerprint += str(args.max_iter)
+    codebook_fingerprint += str(args.epsilon)
+    codebook_fingerprint = codebook_fingerprint.encode('ascii')
+    codebook_filepath = f'codebook-{hashlib.sha256(codebook_fingerprint).hexdigest()[:7]}.pickle'
+    if os.path.isfile(codebook_filepath):
+        msg = 'The codebook already exist. Overwrite?'
+        overwrite = input('{} (y/N) '.format(msg)).lower() in ('y', 'yes')
+        if not overwrite: sys.exit(1)
 
-    print("Constructing codebook...")
-    print("   {:<16} = \"{}\"".format("corpus_file_path", corpus_file_path))
-    print("   {:<16} = {}".format("codebook_size", codebook_size))
-    print("   {:<16} = {}".format("max_iter", max_iter))
-    print("   {:<16} = {}".format("epsilon", epsilon))
+    corpus = utils.load_corpus(args.corpus_filepath)
+    assert len(corpus['pages']) > 0
+    assert len(corpus['pages']) == len(corpus['keypoints']) == len(corpus['descriptors'])
+    for page_keypoints, page_descriptors in zip(corpus['keypoints'], corpus['descriptors']):
+        assert len(page_keypoints) == len(page_descriptors)
 
-    # compute codebook fingerprint, then check if the same codebook was already constructed
-    fingerprint = os.path.basename(corpus_file_path)
-    fingerprint += str(codebook_size)
-    fingerprint += str(max_iter)
-    fingerprint += str(epsilon)
-    fingerprint = fingerprint.encode("utf-8")
+    # run k-means on the descriptors space
+    print('Clustering the descriptors space...')
+    corpus_descriptors_vstack = numpy.vstack(corpus['descriptors'])
 
-    codebook_file_name = "codebook-{}".format(hashlib.sha256(fingerprint).hexdigest()[:7])
-    codebook_file_path = os.path.join(output_directory_path, codebook_file_name)
-    if not force and os.path.isfile(codebook_file_path):
-        print("Codebook already exists and therefore construction is skipped. Use -f to reconstruct it.")
-    else:
-        # construct codebook
-        # load precomputed keypoints and descriptors
-        print("Loading corpus...")
-        corpus = utils.load_corpus(corpus_file_path)
-        assert len(corpus["pages"]) > 0
-        assert len(corpus["pages"]) == len(corpus["keypoints"]) == len(corpus["descriptors"])
-        for page_keypoints, page_descriptors in zip(corpus["keypoints"], corpus["descriptors"]):
-            assert len(page_keypoints) == len(page_descriptors)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, args.max_iter, args.epsilon)
+    compactness, labels, d = cv2.kmeans(
+        corpus_descriptors_vstack, args.codebook_size, None, criteria, attempts=10, flags=cv2.KMEANS_RANDOM_CENTERS)
+    assert len(d) == args.codebook_size
 
-        # run k-means on the descriptors space
-        print("Running k-means on the descriptors space...")
-        corpus_descriptors_vstack = numpy.vstack(corpus["descriptors"])
+    corpus_keypoints_vstack = numpy.asarray(
+        [keypoint for page_keypoints in corpus['keypoints'] for keypoint in page_keypoints])
 
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, max_iter, epsilon)
-        compactness, labels, d = cv2.kmeans(corpus_descriptors_vstack, codebook_size, None,
-                                            criteria, attempts=10, flags=cv2.KMEANS_RANDOM_CENTERS)
-        assert len(d) == codebook_size
+    # construct the codebook of k visual words
+    codebook = {
+        'corpus_filepath': args.corpus_filepath,
+        'codebook_size': args.codebook_size,
+        'max_iter': args.max_iter,
+        'epsilon': args.epsilon,
+        'pages': corpus['pages'],
+        'codewords': list()
+    }
 
-        # construct the codebook of k visual words
-        print("Constructing codebook...")
-        corpus_keypoints_vstack = numpy.asarray(
-            [keypoint for page_keypoints in corpus["keypoints"] for keypoint in page_keypoints])
+    # assign a codeword to each cluster
+    for group in range(len(d)):
+        print(f'Group: {group}')
+        codeword_centroid = d[group]
 
-        codebook = {
-            "corpus_file_path": corpus_file_path,
-            "codebook_size": codebook_size,
-            "max_iter": max_iter,
-            "epsilon": epsilon,
-            "pages": corpus["pages"],
-            "codewords": list()
-        }
+        # find the keypoints that belong to the current cluster and group them in pages
+        codeword_keypoints = dict.fromkeys(codebook['pages'], list())
+        codeword_descriptors = dict.fromkeys(codebook['pages'], list())
 
-        # create a codeword for each k-means group found
-        for group in range(len(d)):
-            codeword_centroid = d[group]
+        curr_page_start_index = curr_page_last_index = 0
+        for i, page_filename in enumerate(corpus['pages']):
+            assert len(corpus['keypoints'][i]) == len(corpus['descriptors'][i])
+            curr_page_last_index = curr_page_start_index + len(corpus['keypoints'][i])
 
-            # find keypoints that belong to the current group, and group them in pages
-            codeword_keypoints = dict.fromkeys(codebook["pages"], list())
-            codeword_descriptors = dict.fromkeys(codebook["pages"], list())
+            curr_page_keypoints = corpus_keypoints_vstack[curr_page_start_index:curr_page_last_index]
+            curr_page_curr_group_keypoints = curr_page_keypoints[labels.ravel()[curr_page_start_index:
+                                                                                curr_page_last_index] == group]
+            codeword_keypoints[page_filename] = curr_page_curr_group_keypoints
 
-            curr_page_start_index = curr_page_last_index = 0
-            for i, page in enumerate(corpus["pages"]):
-                assert len(corpus["keypoints"][i]) == len(corpus["descriptors"][i])
-                curr_page_last_index = curr_page_start_index + len(corpus["keypoints"][i])
+            curr_page_descriptors = corpus_descriptors_vstack[curr_page_start_index:curr_page_last_index]
+            curr_page_curr_group_descriptors = curr_page_descriptors[labels.ravel()[curr_page_start_index:
+                                                                                    curr_page_last_index] == group]
+            codeword_descriptors[page_filename] = curr_page_curr_group_descriptors
 
-                curr_page_keypoints = corpus_keypoints_vstack[curr_page_start_index:curr_page_last_index]
-                curr_page_curr_group_keypoints = curr_page_keypoints[
-                    labels.ravel()[curr_page_start_index:curr_page_last_index] == group]
-                codeword_keypoints[page] = curr_page_curr_group_keypoints
+            curr_page_start_index = curr_page_last_index
 
-                curr_page_descriptors = corpus_descriptors_vstack[curr_page_start_index:curr_page_last_index]
-                curr_page_curr_group_descriptors = curr_page_descriptors[
-                    labels.ravel()[curr_page_start_index:curr_page_last_index] == group]
-                codeword_descriptors[page] = curr_page_curr_group_descriptors
+        codeword = {'d': codeword_centroid, 'keypoints': codeword_keypoints, 'descriptors': codeword_descriptors}
+        codebook['codewords'].append(codeword)
 
-                curr_page_start_index = curr_page_last_index
+    assert len(codebook['codewords']) == args.codebook_size
+    assert len(corpus_keypoints_vstack) == len(corpus_descriptors_vstack)
+    assert sum([len(v) for codeword in codebook['codewords']
+                for v in codeword['keypoints'].values()]) == len(corpus_keypoints_vstack)
+    assert sum([len(v) for codeword in codebook['codewords']
+                for v in codeword['descriptors'].values()]) == len(corpus_descriptors_vstack)
 
-            codeword = {
-                "d": codeword_centroid,
-                "keypoints": codeword_keypoints,
-                "descriptors": codeword_descriptors
-            }
-            codebook["codewords"].append(codeword)
-
-        assert len(codebook["codewords"]) == codebook_size
-        assert len(corpus_keypoints_vstack) == len(corpus_descriptors_vstack)
-        assert sum([len(v) for codeword in codebook["codewords"]
-                    for v in codeword["keypoints"].values()]) == len(corpus_keypoints_vstack)
-        assert sum([len(v) for codeword in codebook["codewords"]
-                    for v in codeword["descriptors"].values()]) == len(corpus_descriptors_vstack)
-
-        # save codebook
-        print("Saving {}...".format(codebook_file_path))
-        utils.save_codebook(codebook, codebook_file_path)  # codebook is modified after this operation
-    return codebook_file_path
+    utils.save_codebook(codebook, codebook_filepath)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-o", default="")
-    parser.add_argument("-f", action="store_true", default=False)
-    parser.add_argument("corpus_file_path")
-    parser.add_argument("codebook_size", type=int)
-    parser.add_argument("max_iter", type=int)
-    parser.add_argument("epsilon", type=float)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('corpus_filepath')
+    parser.add_argument('codebook_size', type=int)
+    parser.add_argument('max_iter', type=int)
+    parser.add_argument('epsilon', type=float)
     args = parser.parse_args()
-    run(vars(args), output_directory_path=args.o, force=args.f)
+    run()
